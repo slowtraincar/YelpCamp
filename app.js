0ab579671613c5d22f551ae3fa24a44652a41ff6
Doug Carter <doug.allen.carter@gmail.com>
2018-05-24 15:44:32 +0000

    update db url

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ var User            = require("./models/user");
 //REQUIRING ROUTES
 var commentRoutes      = require("./routes/comments"),
     campgroundRoutes   = require("./routes/campgrounds"),
-    indexRoutes        = require("./routes/index")
+    indexRoutes        = require("./routes/index");
 
+    
 
 /////////////////////////////////////////
-//mongoose.connect("mongodb://localhost/yelp_camp_v9");
-mongoose.connect("mongodb://dacarter:dbpassword@ds233970.mlab.com:33970/slowtainyelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://dacarter:dbpassword@ds233970.mlab.com:33970/slowtainyelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
